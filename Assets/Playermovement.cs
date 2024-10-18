6caa518692e957efc359bd5081a1d2a9756e9043
Elias Damavandi Jahromi <117083631+grankaswe@users.noreply.github.com>
2024-10-18 13:25:34 +0200

    Initial commit

---
+++ Assets/Playermovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooting : MonoBehaviour
+
+{
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementInput;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _movementInput * _speed;
+    }
+    private void OnMove(InputValue inputValue)
+    {
+       _movementInput = inputValue.Get<Vector2>();
+    }
+}
+
